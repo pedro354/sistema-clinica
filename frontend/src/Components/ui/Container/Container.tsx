93d253e00127639f7f21ef85de627bd9806de09d
Pedro Silva <pedrosilva354@gmail.com>
2026-08-11 23:49:03 -0300

    chore: final

--- frontend/src/Components/ui/Container/Container.tsx
+++ frontend/src/Components/ui/Container/Container.tsx
@@ -6,8 +6,8 @@ interface ContainerProps {
 
 export function Container({ children }: ContainerProps) {
   return (
-    <div className="max-w-7xl mx-auto w-full h-full">
-      {children}
+    <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+            {children}
     </div>
   );
 }
